075c8018d0fa8e9a325a11a4dc365986d58e514c
Christoph van der Fecht <c.vanderfecht@vdsoft.at>
2022-04-26 19:55:46 +0200

    docs: add default documentation

--- src/VDsoft.TextProcessor.Ui/View/MainWindow.xaml.cs
+++ src/VDsoft.TextProcessor.Ui/View/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace VDsoft.TextProcessor.Ui.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
