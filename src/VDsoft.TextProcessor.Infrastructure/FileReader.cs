7df30e6f1c78dd8e6f70257379484330e72e1154
Christoph van der Fecht <c.vanderfecht@vdsoft.at>
2022-04-26 19:10:30 +0200

    refactor: add file reader to access files

---
+++ src/VDsoft.TextProcessor.Infrastructure/FileReader.cs
@@ -0,0 +1,31 @@
+﻿using VDsoft.TextProcessor.BusinessLogic.Infrastructure;
+
+namespace VDsoft.TextProcessor.Infrastructure;
+
+/// <summary>
+/// Access the file system.
+/// </summary>
+/// <seealso cref="IFileReader" />
+public class FileReader : IFileReader
+{
+    /// <inheritdoc/>
+    public Task<string> ReadFileAsync(string filePath, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+        {
+            throw new ArgumentNullException(nameof(filePath), "The provided path must not be null or empty");
+        }
+
+        if (cancellationToken == CancellationToken.None)
+        {
+            throw new ArgumentException("Please provide a valid cancellation token", nameof(cancellationToken));
+        }
+
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"The file '{filePath}' does not exist");
+        }
+
+        return File.ReadAllTextAsync(filePath, cancellationToken);
+    }
+}
---
+++ test/VDsoft.TextProcessor.Infrastructure.Tests/FileReaderTests/ReadAsync.cs
@@ -0,0 +1,81 @@
+﻿namespace VDsoft.TextProcessor.Infrastructure.Tests.FileReaderTests;
+
+public class ReadAsync
+{
+    private const string testFilePath = @"TestData\TextFile.txt";
+    private readonly CancellationTokenSource tokenSource;
+    private readonly FileReader sut;
+
+    public ReadAsync()
+    {
+        tokenSource = new();
+        sut = new();
+    }
+
+    [Fact]
+    public async Task ReadAsync_FileIsPresentAndHasContent_ContentOfTheFileIsRead()
+    {
+        // Act
+        var actual = await sut.ReadFileAsync(testFilePath, tokenSource.Token);
+
+        // Assert
+        actual.Should().NotBeNullOrWhiteSpace().And.Be("Content of the test file should be read");
+    }
+
+    [Fact]
+    public async Task ReadAsync_ProvidedFileNotFound_FileNotFoundException()
+    {
+        // Arrange
+        var filePath = @"X:\Shall\Not\Exist.txt";
+        var expected = new FileNotFoundException($"The file '{filePath}' does not exist");
+
+        // Act
+        Func<Task> invokation = async () => await sut.ReadFileAsync(filePath, tokenSource.Token);
+
+        // Assert
+        await invokation.Should().ThrowAsync<FileNotFoundException>();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task ReadAsync_PathIsNull_ArgumentNullException(string path)
+    {
+        // Arrange
+        var expected = new ArgumentNullException("filePath", "The provided path must not be null or empty");
+
+        // Act
+        Func<Task> invokation = async () => await sut.ReadFileAsync(path, tokenSource.Token);
+
+        // Assert
+        await invokation.Should().ThrowAsync<ArgumentNullException>().WithMessage(expected.Message);
+    }
+
+    [Fact]
+    public async Task ReadAsync_CancellationTokenNoneProvided_ArgumentException()
+    {
+        // Arrange
+        var token = CancellationToken.None;
+        var expected = new ArgumentException("Please provide a valid cancellation token", "cancellationToken");
+
+        // Act
+        Func<Task> invokation = async () => await sut.ReadFileAsync(testFilePath, token);
+
+        // Assert
+        await invokation.Should().ThrowAsync<ArgumentException>().WithMessage(expected.Message);
+    }
+
+    [Fact]
+    public async Task ReadAsync_OperationIsCanceled_OperationCanceledException()
+    {
+        // Arrange
+        tokenSource.Cancel();
+
+        // Act
+        Func<Task> invokation = async () => await sut.ReadFileAsync(testFilePath, tokenSource.Token);
+
+        // Assert
+        await invokation.Should().ThrowAsync<OperationCanceledException>();
+    }
+}
