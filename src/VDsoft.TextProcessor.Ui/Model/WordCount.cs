337b8f993783adffbe396ca999587fffb52297ee
Christoph van der Fecht <c.vanderfecht@vdsoft.at>
2022-04-27 22:37:06 +0200

    refactor: add ui implementation

---
+++ src/VDsoft.TextProcessor.Ui/Model/WordCount.cs
@@ -0,0 +1,8 @@
+﻿namespace VDsoft.TextProcessor.Ui.Model;
+
+/// <summary>
+/// Represents a word with the count how often it occurs in a text.
+/// </summary>
+/// <param name="Word">The found word.</param>
+/// <param name="Count">The count how often the word occurs.</param>
+public record WordCount(string Word, int Count);
--- src/VDsoft.TextProcessor.Ui/View/MainWindow.xaml.cs
+++ src/VDsoft.TextProcessor.Ui/View/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VDsoft.TextProcessor.Ui.View
 {
@@ -26,6 +14,22 @@ namespace VDsoft.TextProcessor.Ui.View
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Multiselect = false,
+                RestoreDirectory = true
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                TextBoxFilePath.Text = dialog.FileName;
+            }
         }
     }
 }
